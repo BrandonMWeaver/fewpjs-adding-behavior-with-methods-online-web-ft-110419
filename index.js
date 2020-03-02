3cfdea7b0cdca730b654a5c0ca99fd71a15b1600
Brandon Weaver <a7xncob@gmail.com>
2020-03-02 02:23:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ class Cat {
     this.name = name;
     this.sex = sex;
   }
+  
+  speak() {
+    return `${this.name} says meow!`;
+  }
 }
 
 class Dog {
@@ -10,11 +14,21 @@ class Dog {
     this.name = name;
     this.sex = sex;
   }
+  
+  speak() {
+    return `${this.name} says woof!`;
+  }
 }
 
 class Bird {
   constructor(name, sex) {
     this.name = name;
     this.sex = sex;
+  }
+  
+  speak() {
+    if (this.sex === "male")
+      return `It's me! ${this.name}, the parrot!`;
+    return `${this.name} says squawk!`;
   }
 }
